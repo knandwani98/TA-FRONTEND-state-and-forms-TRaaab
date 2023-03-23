c68317b6aff1a2975f01c9908e5b1c9a7c569d41
Kushal Nandwani <kushalnandwani98@gmail.com>
2023-03-23 20:58:52 +0530

    State

---
+++ block-BRaabd/buttons/src/Component/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <section className="container">
+        <img src="" alt="" />
+      </section>
+    );
+  }
+}
+export default Hero;
