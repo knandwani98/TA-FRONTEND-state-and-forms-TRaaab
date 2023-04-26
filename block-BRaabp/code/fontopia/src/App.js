8c7bb52b46333613db0b56ff9acf041b86f6080b
Kushal Nandwani <kushalnandwani98@gmail.com>
2023-04-26 18:07:45 +0530

    fontopia

---
+++ block-BRaabp/code/fontopia/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
